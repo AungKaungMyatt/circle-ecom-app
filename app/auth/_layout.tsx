5482faca773c3fb2f19208f5178bf1c86c04ca68
Aung Kaung Myatt <aungkaungmyatt11@gmail.com>
2025-10-24 11:11:52 +0630

    feat(clean route):export function for cleaner way

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function AuthLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="let-you-in" />
+      <Stack.Screen name="sign-in" />
+      <Stack.Screen name="sign-up" />
+    </Stack>
+  );
+}
