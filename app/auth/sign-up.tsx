b76121234dab83d1e36d470ecdc6cda90a076e67
Aung Kaung Myatt <aungkaungmyatt11@gmail.com>
2025-10-28 10:45:10 +0630

    refactor(imports): changed imports

--- app/auth/sign-up.tsx
+++ app/auth/sign-up.tsx
@@ -1,13 +1,2 @@
-import { View, Text } from "react-native";
-import { useTheme } from "../../src/core/providers/ThemeProvider";
-
-export default function SignUp() {
-  const t = useTheme();
-  return (
-    <View style={{ flex:1, backgroundColor:t.colors.bg, padding:16 }}>
-      <Text style={{ color:t.colors.text, fontSize:24, fontWeight:"700" }}>
-        Sign up
-      </Text>
-    </View>
-  );
-}
+import SignUpContainer from "../../src/features/auth/container/SignUpContainer";
+export default SignUpContainer;
