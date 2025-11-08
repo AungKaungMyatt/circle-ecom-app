3c740e4e4dde0dc5b343da7184fd7f1b3ca58dc8
Aung Kaung Myatt <aungkaungmyatt11@gmail.com>
2025-11-08 09:34:55 +0630

    refactor(axios): update the initalization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from "expo-router";
 import { ThemeProvider } from "@core/providers/ThemeProvider";
+import { useAuth } from "@/core/store/auth";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+
+  const hydrate = useAuth((s) => s.hydrate);
+  useEffect(() => { hydrate(); }, [hydrate]);
+  
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }} />
