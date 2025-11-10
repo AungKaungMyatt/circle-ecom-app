06ad537e3219f2d4a5457b2a5e69a98a1193ab41
Aung Kaung Myatt <aungkaungmyatt11@gmail.com>
2025-11-10 09:52:49 +0630

    refactor(email): adding verify email screen

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -6,6 +6,7 @@ export default function AuthLayout() {
       <Stack.Screen name="let-you-in" />
       <Stack.Screen name="sign-in" />
       <Stack.Screen name="sign-up" />
+      <Stack.Screen name="verify-email" />
     </Stack>
   );
 }
